242f8cae9213bc6222ccddfb338b35f00ee88195
Rob <fadedrob@users.noreply.github.com>
2022-02-16 04:23:51 -0800

    [ModifyPoints] Change reading/writing charstats.txt

--- ModifyPoints/mod.js
+++ ModifyPoints/mod.js
@@ -1,23 +1,20 @@
+const charstatsFilename = "global\\excel\\charstats.txt";
+const charstats = D2RMM.readTsv(charstatsFilename);
+
 if (config.statCheck) {
-  const charstatsFilename = "global\\excel\\charstats.txt";
-  const charstats = D2RMM.readTsv(charstatsFilename);
   charstats.rows.forEach((row) => {
-    if (row["StatPerLevel"] !== "") {
+    if (row.StatPerLevel !== "") {
       row.StatPerLevel = config.statPoints;
     }
   });
-
-  D2RMM.writeTsv(charstatsFilename, charstats);
 }
 
 if (config.skillCheck) {
-  const charstatsFilename = "global\\excel\\charstats.txt";
-  const charstats = D2RMM.readTsv(charstatsFilename);
   charstats.rows.forEach((row) => {
-    if (row["SkillsPerLevel"] !== "") {
+    if (row.SkillsPerLevel !== "") {
       row.SkillsPerLevel = config.skillPoints;
     }
   });
-
-  D2RMM.writeTsv(charstatsFilename, charstats);
 }
+
+D2RMM.writeTsv(charstatsFilename, charstats);
